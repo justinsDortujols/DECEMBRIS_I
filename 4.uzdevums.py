91d4b23b7e4d41797ccddd79fbb9a8370441ed45
justinsDortujols <justins.dortujols@grobinasskola.lv>
2023-12-11 16:48:13 +0200

    5.uzdevums1.0 un 4.uzdevums1.0 saglabats

---
+++ 4.uzdevums.py
@@ -0,0 +1,3 @@
+ievadit_sk=int(input("Ievadīt skaitli: "))
+for i in ievadit_sk:
+    pass
---
+++ 5.uzdevums.py
@@ -0,0 +1,9 @@
+import datetime
+
+if time()>= 12:
+    print("Labdien!")
+elif time()<= 12:
+    print("Labrīt!")
+else:
+    print("Labvakar!")
+
